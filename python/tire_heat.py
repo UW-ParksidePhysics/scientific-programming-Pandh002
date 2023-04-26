f09dcf63498b2fb2a463fb52123c839895322409
Pandh002 <pandh002@rangers.uwp.edu>
2023-04-26 11:59:15 -0500

    pyhton project

--- python/tire_heat.py
+++ python/tire_heat.py
@@ -39,10 +39,13 @@ def convert_units(input_value, input_units, output_units):
     miles_to_meters = 1609.344
     hours_to_seconds = 3600
     pounds_to_kilograms = 0.45359237
+    grams_to_kilograms = 1e-3
     if input_units == 'mph' and output_units == 'm/s':
         output_value = input_value * miles_to_meters/hours_to_seconds
     elif input_units == 'lbs' and output_units == 'kg':
         output_value = input_value * pounds_to_kilograms
+    elif input_units == "J/g°C" and output_units == 'J/kg K':
+        output_value = input_value / grams_to_kilograms
     return output_value
 
 
@@ -53,26 +56,39 @@ if __name__  == "__main__":
     ratings, load_indexes = extract_tire_data(tire_data_url)
     tire_speed_ratings = ratings['Maximum speed (MPH)'][0:7].astype('float').to_numpy()
     tire_load = load_indexes['Load (lbs)'].astype('float').to_numpy()
+
     tire_speed_ratings = convert_units(tire_speed_ratings, 'mph', 'm/s')
     tire_load = convert_units(tire_load, 'lbs', 'kg')
 
+    print(f'v = {tire_speed_ratings[-1]}')
+
   # https://www.matweb.com/search/datasheet_print.aspx?matguid=6588439546ac4492965c894ddff3f5da
     tire_specific_heat = 0.440  ## J/g°C
+    tire_specific_heat = convert_units(tire_specific_heat, "J/g°C", "J/kg K")
+    print(f'c = {tire_specific_heat}')
     tire_mass = convert_units(18, 'lbs', 'kg')
+    print(f'm_l = {tire_load[-1]}')
+    print(f'm_T = {4*tire_mass}')
+    print(f'm_k/m_T = {(tire_load[-1]+4*tire_mass)/(4*tire_mass)}')
+    print(f'm_k/m_T/c = {(tire_load[-1] + 4 * tire_mass) / (4 * tire_mass*tire_specific_heat)}')
+    print(f'v^2 = {tire_speed_ratings[-1]**2}')
+    print(f'0.5 m_k/m_T/c v^2 = {0.5*(tire_load[-1] + 4 * tire_mass)*tire_speed_ratings[-1]**2 / (4 * tire_mass*tire_specific_heat)}')
+
 
     starting_temperature = 273.15 + 25
     for load in tire_load:
-        total_mass = 4 * (tire_mass + load)
-        print(total_mass)
-        temperatures = calculate_final_temperature([tire_mass, total_mass], tire_specific_heat, tire_speed_ratings,
+        total_mass = 4 * tire_mass + load
+        #print(total_mass)
+        temperatures = calculate_final_temperature([4*tire_mass, total_mass], tire_specific_heat, tire_speed_ratings,
                                                    starting_temperature)
 
         plt.plot(tire_speed_ratings, temperatures-starting_temperature, label=f'{load:.0f}')
-# label the x-axis using xlabel, label y-axis using ylabel, create a le,gend using
+# label the x-axis using xlabel, label y-axis using ylabel, create a legend using
     #labelequals in play command plt.function
     plt.xlabel("Velocity at the start of the skid [m/s]")
     plt.ylabel("Temperature change of tire [K]")
 
-    plt.legend()
+    plt.legend(title="Load (kg)", ncols=2)
     plt.show()
+
 
