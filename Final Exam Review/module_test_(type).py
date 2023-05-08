8bd18aa92fd13d70ad8f57cdd673ee63cd188c77
Trevor Pandher <124001627+Pandh002@users.noreply.github.com>
2023-05-08 16:11:19 -0500

    Update module_test_(type).py

--- Final Exam Review/module_test_(type).py
+++ Final Exam Review/module_test_(type).py
@@ -1,3 +1,5 @@
+__author__ = 'Trevor Pandher'
+
 modules = ['read_two_columns', 'calculate_bivariate_statistics', 'calculate_quadratic_fit', 'fit_curve_array', 'plot_data_with_fit', 'calculate_lowest_eigenvectors']
 for module in modules:
     with open(module+'.py') as module_file:
